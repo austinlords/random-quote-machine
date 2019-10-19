de7745b8b88a0950db78c9a9677dcb1746d51a68
Austin Lords <austinlords@outlook.com>
2019-10-19 13:58:57 -0700

    Updates

---
+++ precache-manifest.ad69e5790dafa67cf418f6514f37698d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f181a12f5cc7ba0ff301178fa5deaf",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "b09a1529022dcd19673a",
+    "url": "/random-quote-machine/static/css/main.f57d1466.chunk.css"
+  },
+  {
+    "revision": "cdde593eeecaa4a6c150",
+    "url": "/random-quote-machine/static/js/2.c033e98b.chunk.js"
+  },
+  {
+    "revision": "b09a1529022dcd19673a",
+    "url": "/random-quote-machine/static/js/main.84740195.chunk.js"
+  },
+  {
+    "revision": "a1217defecffe873141b",
+    "url": "/random-quote-machine/static/js/runtime-main.6d700caf.js"
+  }
+]);
